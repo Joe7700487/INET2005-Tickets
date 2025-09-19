ba69ce1654ffceb99c45d391a51d518e4bf3803d
Joe7700487 <w0500154@nscc.ca>
2025-09-19 13:17:38 -0300

    Created event model

---
+++ AwesomeTickets/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace AwesomeTickets.Models
+{
+    public class Event
+    {
+        int EventId { get; set; }
+        String EventTitle { get; set; } = string.Empty;
+        String EventDescription { get; set; } = string.Empty;
+        String EventCategory { get; set; } = string.Empty;
+        DateTime EventDate { get; set; }
+        String EventLocation { get; set; } = string.Empty;
+        String EventOwner { get; set; } = string.Empty;
+        DateTime DateCreated { get; set; }
+    }
+}
+
+
+//date, time, location, owner (who is running the event), date and time record created.
