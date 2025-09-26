8b9a16451dacb2df5cb21b3173076ec078327726
Joe7700487 <w0500154@nscc.ca>
2025-09-26 13:36:07 -0300

    scaffolded models and created db

--- AwesomeTickets/Controllers/HomeController.cs
+++ AwesomeTickets/Controllers/HomeController.cs
@@ -15,11 +15,21 @@ namespace AwesomeTickets.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
+            //List<Listing> listings = new List<Listing>();
+            //Listing listing1 = new Listing();
+            //listing1.ListingId = 1;
+            //listing1.ListingTitle = "show title";
+            //listing1.ListingDate = new DateTime(2025, 9, 19, 13, 58, 0); // Sept 19, 2025 1:58pm;
+            //listing1.DateCreated = DateTime.Now;
+            //listings.Add(listing1);
+            //Listing listing2 = new Listing();
+            //listing2.ListingId = 2;
+            //listing2.ListingTitle = "show title 2";
+            //listing2.ListingDate = new DateTime(2026, 9, 19, 13, 58, 0); // Sept 19, 2025 1:58pm;
+            //listing2.DateCreated = DateTime.Now;
+            //listings.Add(listing2);
+            //_logger.Log(LogLevel.Information, "Number of shows: " + listings.Count);
+            //return View(listings);
             return View();
         }
 
