732e8e2c397ef33156bda5bec90dc8d7f48379d5
Joe7700487 <w0500154@nscc.ca>
2025-09-26 11:41:04 -0300

    removed boiler plate and added show and ticket models

---
+++ AwesomeTickets/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AwesomeTickets.Models
+{
+    public class Ticket
+    {
+        // primary key
+        public int TicketId { get; set; }
+        public string ShowTitle { get; set; } = string.Empty;
+        public DateTime ShowDate { get; set; }
+        public string ShowLocation { get; set; } = string.Empty;
+        // foreign key
+        public int ShowId { get; set; }
+        // navigation property
+        public Show? Show { get; set; }
+    }
+}
