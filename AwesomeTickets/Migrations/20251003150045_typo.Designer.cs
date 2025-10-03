dbfa0bf64423fb5995e8f4007a5912c4dd68e5b4
Joe7700487 <w0500154@nscc.ca>
2025-10-03 12:05:20 -0300

    Polished web pages

--- AwesomeTickets/Migrations/20251003150045_typo.Designer.cs
+++ AwesomeTickets/Migrations/20251003150045_typo.Designer.cs
@@ -4,6 +4,7 @@ using AwesomeTickets.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AwesomeTickets.Migrations
 {
     [DbContext(typeof(AwesomeTicketsContext))]
-    partial class AwesomeTicketsContextModelSnapshot : ModelSnapshot
+    [Migration("20251003150045_typo")]
+    partial class typo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AwesomeTickets/Models/Listing.cs
+++ AwesomeTickets/Models/Listing.cs
@@ -1,15 +1,25 @@
-﻿namespace AwesomeTickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwesomeTickets.Models
 {
     public class Listing
     {
         // primary key
+        [Display(Name = "ID")]
         public int ListingId { get; set; }
+        [Display(Name = "Title")]
         public string ListingTitle { get; set; } = string.Empty;
+        [Display(Name = "Description")]
         public string ListingDescription { get; set; } = string.Empty;
+        [Display(Name = "Category")]
         public string ListingCategory { get; set; } = string.Empty;
+        [Display(Name = "Date")]
         public DateTime ListingDate { get; set; }
+        [Display(Name = "Location")]
         public string ListingLocation { get; set; } = string.Empty;
+        [Display(Name = "Owner")]
         public string ListingOwner { get; set; } = string.Empty;
+        [Display(Name = "Date Created")]
         public DateTime DateCreated { get; set; }
         // navigation property
         public List<Category>? Categories { get; set; }
